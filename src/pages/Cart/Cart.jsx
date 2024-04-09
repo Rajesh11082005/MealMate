1bb9fe6a5e796e3390db08e3c93d0fab2f220089
Rajesh11082005 <rajesh007.2005@gmail.com>
2024-04-09 19:26:20 +0530

    update code for counter and empty cart conditional UI

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,84 +1,109 @@
 import React, { useContext } from 'react'
 import "./Cart.css"
 import { StoreContext } from '../../context/StoreContext'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { assets } from '../../assets/assets'
 
 
 const Cart = () => {
 
-    const { cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext)
+    const { cartItems, food_list, removeFromCart, getTotalCartAmount, addToCart } = useContext(StoreContext)
 
     const navigate = useNavigate()
 
+    console.log(getTotalCartAmount());
+
     return (
-        <div className="cart">
-            <div className="cart-items">
-                <div className="cart-items-title">
-                    <p>Items</p>
-                    <p>Title</p>
-                    <p>Price</p>
-                    <p>Quantity</p>
-                    <p>Total</p>
-                    <p>Remove</p>
-                </div>
 
-                <br />
-                <hr />
-
-                {
-                    food_list.map((item, index) => {
-                        if (cartItems[item._id] > 0) {
-                            return (
-                                <div>
-                                    <div key={index} className="cart-items-title cart-items-item">
-                                        <img src={item.image} alt="img" />
-                                        <p>{item.name}</p>
-                                        <p>${item.price}</p>
-                                        <p>{cartItems[item._id]}</p>
-                                        <p>${item.price * cartItems[item._id]}</p>
-                                        <p onClick={() => removeFromCart(item._id)} className='cross'>X</p>
-                                    </div>
-                                    <hr />
+        <>
+            {
+                (getTotalCartAmount()) === 0 ?
+                    (
+                        <div className="empty-cart">
+                            <img src={assets.emptycart} alt="empty-cart" />
+                            <Link to="/#explore-menu"><button>Explore Menu</button></Link>
+                        </div>
+                    )
+                    :
+                    (
+                        <div className="cart">
+                            <div className="cart-items">
+                                <div className="cart-items-title">
+                                    <p>Items</p>
+                                    <p>Title</p>
+                                    <p>Price</p>
+                                    <p>Quantity</p>
+                                    <p>Total</p>
+                                    <p>Remove</p>
                                 </div>
-                            )
-                        }
-                    })
-                }
 
-            </div>
+                                <br />
+                                <hr />
 
-            <div className="cart-bottom">
-                <div className="cart-total">
-                    <h2>Cart Totals</h2>
-                    <div>
-                        <div className="cart-total-details">
-                            <p>SubTotal</p>
-                            <p>${getTotalCartAmount()}</p>
+                                {
+                                    food_list.map((item, index) => {
+                                        if (cartItems[item._id] > 0) {
+                                            return (
+                                                <div key={index}>
+                                                    <div key={index} className="cart-items-title cart-items-item">
+                                                        <img src={item.image} alt="img" />
+                                                        <p>{item.name}</p>
+                                                        <p>${item.price}</p>
+                                                        <p>{cartItems[item._id]}</p>
+                                                        <p>${item.price * cartItems[item._id]}</p>
+                                                        <div className='cross'>
+                                                            <div className="food-item-counter">
+                                                                <img onClick={() => removeFromCart(item._id)} src={assets.remove_icon_red} alt="remove" />
+                                                                <p>{cartItems[item._id]}</p>
+                                                                <img onClick={() => addToCart(item._id)} src={assets.add_icon_green} alt="add" />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <hr />
+                                                </div>
+                                            )
+                                        }
+                                    })
+                                }
+
+                            </div>
+
+                            <div className="cart-bottom">
+                                <div className="cart-total">
+                                    <h2>Cart Totals</h2>
+                                    <div>
+                                        <div className="cart-total-details">
+                                            <p>SubTotal</p>
+                                            <p>${getTotalCartAmount()}</p>
+                                        </div>
+                                        <hr />
+                                        <div className="cart-total-details">
+                                            <p>Delivery Fee</p>
+                                            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+                                        </div>
+                                        <hr />
+                                        <div className="cart-total-details">
+                                            <p>Total</p>
+                                            <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
+                                        </div>
+                                    </div>
+                                    <button onClick={() => navigate("/order")}>Proceed to checkout</button>
+                                </div>
+                                <div className="cart-promo-code">
+                                    <div>
+                                        <p>If you have a promo code, Enter it here</p>
+                                        <div className='cart-promocode-input'>
+                                            <input type="text" placeholder='promo code' />
+                                            <button>Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <hr />
-                        <div className="cart-total-details">
-                            <p>Delivery Fee</p>
-                            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
-                        </div>
-                        <hr />
-                        <div className="cart-total-details">
-                            <p>Total</p>
-                            <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
-                        </div>
-                    </div>
-                    <button onClick={() => navigate("/order")}>Proceed to checkout</button>
-                </div>
-                <div className="cart-promo-code">
-                    <div>
-                        <p>If you have a promo code, Enter it here</p>
-                        <div className='cart-promocode-input'>
-                            <input type="text" placeholder='promo code' />
-                            <button>Submit</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    )
+            }
+
+        </>
     )
 }
 
--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,53 +1,68 @@
 import React, { useContext } from 'react'
 import "./PlaceOrder.css"
 import { StoreContext } from '../../context/StoreContext'
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const PlaceOrder = () => {
-    const {getTotalCartAmount} = useContext(StoreContext)
+    const { getTotalCartAmount } = useContext(StoreContext)
     return (
-        <form className='place-order'>
-            <div className="place-order-left">
-                <p className="title">Delivery Information</p>
-                <div className="multi-feilds">
-                    <input type="text" placeholder='First name' />
-                    <input type="text" placeholder='Last name' />
-                </div>
-                <input type="email" placeholder='Email address' />
-                <input type="text" placeholder='Street' />
-                <div className="multi-feilds">
-                    <input type="text" placeholder='City' />
-                    <input type="text" placeholder='State' />
-                </div>
-                <div className="multi-feilds">
-                    <input type="text" placeholder='Pin code' />
-                    <input type="text" placeholder='Country' />
-                </div>
-                <input type="text" placeholder='Phone' />
+        <>
+            {
+                (getTotalCartAmount()) === 0 ? 
+                (
+                    <div className="empty-cart">
+                        <img src={assets.emptycart} alt="empty-cart" />
+                        <Link to="/#explore-menu"><button>Explore Menu</button></Link>
+                    </div>
+                )
+                :
+                (<form className='place-order'>
+                    <div className="place-order-left">
+                        <p className="title">Delivery Information</p>
+                        <div className="multi-feilds">
+                            <input type="text" placeholder='First name' />
+                            <input type="text" placeholder='Last name' />
+                        </div>
+                        <input type="email" placeholder='Email address' />
+                        <input type="text" placeholder='Street' />
+                        <div className="multi-feilds">
+                            <input type="text" placeholder='City' />
+                            <input type="text" placeholder='State' />
+                        </div>
+                        <div className="multi-feilds">
+                            <input type="text" placeholder='Pin code' />
+                            <input type="text" placeholder='Country' />
+                        </div>
+                        <input type="text" placeholder='Phone' />
 
-            </div>
-            <div className="place-order-right">
-                <div className="cart-total">
-                    <h2>Cart Totals</h2>
-                    <div>
-                        <div className="cart-total-details">
-                            <p>SubTotal</p>
-                            <p>${getTotalCartAmount()}</p>
-                        </div>
-                        <hr />
-                        <div className="cart-total-details">
-                            <p>Delivery Fee</p>
-                            <p>{getTotalCartAmount() === 0 ? 0 : 2}</p>
-                        </div>
-                        <hr />
-                        <div className="cart-total-details">
-                            <p>Total</p>
-                            <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
+                    </div>
+                    <div className="place-order-right">
+                        <div className="cart-total">
+                            <h2>Cart Totals</h2>
+                            <div>
+                                <div className="cart-total-details">
+                                    <p>SubTotal</p>
+                                    <p>${getTotalCartAmount()}</p>
+                                </div>
+                                <hr />
+                                <div className="cart-total-details">
+                                    <p>Delivery Fee</p>
+                                    <p>{getTotalCartAmount() === 0 ? 0 : 2}</p>
+                                </div>
+                                <hr />
+                                <div className="cart-total-details">
+                                    <p>Total</p>
+                                    <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
+                                </div>
+                            </div>
+                            <button>Proceed to payment</button>
                         </div>
                     </div>
-                    <button>Proceed to payment</button>
-                </div>
-            </div>
-        </form>
+                </form>)
+            }
+
+        </>
     )
 }
 
